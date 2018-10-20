e24a100c762ac9690e1ffd899ec90258cf9baf59
unknown <wer4ty@gmail.com>
2018-10-20 19:56:30 +0300

    Init bfs algorithm

--- js/bfs.js
+++ js/bfs.js
@@ -11,5 +11,32 @@ let G = [
 	{"name": 'g', "neighbors": ['f'] }
 ];
 
-//2. Algorithms BFS (Bread First Search) 
 
+//2. Algorithms BFS (Bread First Search)  
+//get 2 parameters graph G and node s
+function bsf(G, s) {
+
+	// 2.1 Create simple API of queue for algorithms needs
+	let Q = {
+    	values : [],
+    	enqueue : function(obj) { values.unshift(obj); },
+    	dequeue : function() { if (values.lenth === 0) { return null; }
+    	else return  values.shift(); }
+	}
+
+
+	// 2.2 Init
+	let d = [];
+	let color = [];
+	let pi = [];
+	for (var i =0; i<G.length; i++) {
+		d[i] = Infinity;
+		color[i] = 'G' // gray
+		pi[i] = null;
+	}
+
+	
+}
+
+
+bsf(G, G[0]);

af750bdea7e64f7127b6dc3248c1fa65963a3440
wer4ty <roma0152@bitbucket.org>
2018-12-10 19:06:15 +0200

    DFS

---
+++ js/dfs.js
@@ -0,0 +1,70 @@
+// 0. Graph object represent as list of members
+ 
+let G = [
+	{"name": 'a', "neighbors": ['d', 'g'] },
+	{"name": 'b', "neighbors": ['a'] },
+	{"name": 'c', "neighbors": ['b'] },
+	{"name": 'd', "neighbors": ['g', 'h'] },
+	{"name": 'e', "neighbors": ['f', 'h'] },
+	{"name": 'f', "neighbors": ['c', 'e'] },
+	{"name": 'g', "neighbors": ['d'] },
+	{"name": 'h', "neighbors": ['g'] },
+];
+
+let time;
+
+//1. Algorithms DFS (Depth First Search)  
+//get 1 parameters graph G and node s
+function dsf(G) {
+
+	// 1.1 Init
+	for (let i =0; i<G.length; i++) {
+		G[i].d = 0;
+		G[i].f = 0;
+		G[i].color = 'W' // white
+		G[i].pi = null;
+	}
+
+	time = 0;
+
+	// 1.2 Alrorithms cycle
+	G.forEach(function(v) {
+		if (v.color == 'W') {
+		DFS_Visit(G, v);
+		}
+	});
+}
+
+// 1.3 Recursively function DFS_Visit
+function DFS_Visit(G,u) {
+	u.color = 'G'; // gray
+	time++;
+	u.d = time;
+
+	u.neighbors.forEach(function(v) {
+		let tmp = search(G, v);
+		if (tmp.color == 'W') {
+			tmp.pi = u;
+			DFS_Visit(G, tmp);
+		}
+	});
+
+	u.color = 'B';
+	time++;
+	u.f = time;
+}
+
+//search in array helper function
+function search(arr, value) {
+	for (let i =0; i<arr.length; i++) {
+		if (arr[i].name === value) {
+			return arr[i];
+		}
+	}
+	return null;
+}
+
+$(function() {
+	dsf(G);
+	console.log(G);
+});

05ab25a4e9d9c3b1c6027d92ca019df317b5bddf
wer4ty <roma0152@bitbucket.org>
2018-12-19 15:21:01 +0200

    Floyd warshall finish

--- js/floyd_warshall.js
+++ js/floyd_warshall.js
@@ -14,42 +14,50 @@ let G = [
 function Floyd_Warshall(G) {
 	// 1.1 Init
 
-	let D = new Array();
-	D[0] = new Array();
-	D[0][1] = new Array();
-
+	let D = [];
 	let PI = [];
 
+	D[0] = [];
 	for (let i=0; i<G.length; i++) {
+		D[0][i] = [];
+		PI[i] = [];
 		for (let j=0; j<G.length; j++) {
 			if (G[i][j] != Infinity) {
 				D[0][i][j] = G[i][j];
 				PI[i][j] = i;
 			}
 			else {
-				D[0][i,j] = Infinity;
-				PI[i,j] = "NIL";
+				D[0][i][j] = Infinity;
+				PI[i][j] = "NIL";
 			}
 		}
 	}
+	console.log(D[0]);
 
 	// main loop algorithm
+	let t1, t2,t3;
+
 	for(let k=1; k<G.length; k++) {
+		D[k] = [];
 		for (let i=0; i<G.length; i++) {
+			D[k][i] = [];
 			for (let j=0; j<G.length; j++) {
-				if(D[k-1][i][j] <= D[k-1][i][k] + D[k-1][k][j]) {
-					D[k][i][j] = D[k-1][i][j];
-					PI[i,j] =  i;
+				t1 = D[k-1][i][j];
+				t2 = D[k-1][i][k];
+				t3 =  D[k-1][k][j];
+				if(t1 <= t2 + t3) {
+					D[k][i][j] = t1;
+					PI[i][j] =  i;
 				}
 				else {
-					D[k][i][j] = D[k-1][i][k] + D[k-1][k][j];
+					D[k][i][j] = t2 + t3;
 					PI[i,j] =  i;
 				}
 			}
 		}
+		console.log(D[k]);
 	}
 
-	console.log(D);
 	return D;
 }
 

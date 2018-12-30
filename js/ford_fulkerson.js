745e4373f1e0996a7879d4f0c6812708126bb6bd
wer4ty <roma0152@bitbucket.org>
2018-12-30 22:07:35 +0200

    Find path function and fine minimum bottle neck Floyd Warshall algorithm network flow

--- js/ford_fulkerson.js
+++ js/ford_fulkerson.js
@@ -30,34 +30,137 @@ function Ford_Fulkerson(s,t) {
 	}
 	
 	// 2. Init
-	let f = [];
+	let flow1 = [], flow2 = [], cf1 = [], cf2 = [];
+	
 	E.forEach(function(e){
-		f.push( {v1: e.v1, v2: e.v2, c: 0} );
-		f.push( {v1: e.v2, v2: e.v1, c: 0} );
+		flow1.push( {v1: e.v1, v2: e.v2, val: 0} );
+		flow2.push( {v1: e.v2, v2: e.v1, val: 0} );
+		cf1.push( {v1: e.v1, v2: e.v2, val: e.w} );
+		cf2.push( {v1: e.v2, v2: e.v1, val: -e.w} );
 	});
 	
 	let N = [], Nf = [];
 	G.forEach(function(v){ Nf.push(v); N.push(v); });
 
-	// 3. Find Path in Nf from s to t
+	// 3. Find Path in Nf from s to t (BFS with improvment)
 	// start index -> 0, destination index -> last elementh in array Nf
 	function findPath(start, destination) {
-		let path = [], Q = [];
+		let Q = [], G_tag = [], path = [];
+		let destination_index = -1;
 		
-		for ( let i=0; i<Nf.length-1; i++) {
-			path.push(Nf[i]);
-			
-			while(path.length > 0) {
-				for ( let i=0; i<Nf.length-1; i++) {
-			}
+		Nf.forEach(function(vertex){
+			let v_tag = {v: vertex.v, neiborhoods:vertex.neiborhoods, color:'W', d:Infinity, pi:undefined}
+			G_tag.push(v_tag); 
+			});
+		
+		G_tag[0].d = 0;
+		G_tag[0].color = 'G';
+		Q.push(G_tag[0]);
+		
+		while (Q.length > 0) {
+			let u = Q.shift();
+			u.neiborhoods.forEach(function(ver){
+				let t_ind = search_in_array(G_tag, ver.n);
+				let t = G_tag[t_ind];
+				
+				if (t.color === 'W') {
+					t.color = 'G';
+					t.d = u.d + 1;
+					t.pi = u.v;
+					Q.push(t); 
+					
+					
+					if (t.v == destination.v) {
+						let x = t;
+						while (true) {
+							let x_ind = search_in_array(G_tag, x.pi);
+							path.push(x.v);
+							if (x_ind != -1) {
+								x = G_tag[x_ind];
+							}
+							else {
+								break;
+							}
+						}
+					}
+				}
+			});
+			u.color = 'B';
 		}
-		
+	return 	path;	
 	}
 	
-	findPath(s,t);
+	
+	//4. find bottle neck edge in Path with minimum capacity
+	function find_min_capacity_edge(path) {
+		let c_min = Infinity;
+		for (let i=0; i < path.length-1; i++) {
+			let v1 = path[i+1];
+			let v2 = path[i];
+			
+			E.forEach(function(e){
+				if (e.v1 == v1 && e.v2 == v2) {
+					if (e.w < c_min)  c_min = e.w;
+				}
+			});
+		}
+	return c_min;
+	}
+	
+	// dependly on previous calculate flow rebild edges in network Nf
+	function rebild_Nf() {
+	
+	}
+	
+	let P = [];
+	//while (true) {
+		P = findPath(s,t);
+		if (P.length > 0) {
+			console.log(P);
+			
+			let cf_P = find_min_capacity_edge(P);
+			console.log(cf_P);
+			
+			for (let i=0; i < P.length-1; i++) {
+			let v1 = P[i+1];
+			let v2 = P[i];
+			
+			
+			flow1.forEach(function(f){ 
+				if (f.v1 == v1 && f.v2 == v2) {
+				f.val = f.val + cf_P; 
+				}
+			});
+			
+			flow2.forEach(function(f){ 
+				if (f.v2 == v1 && f.v1 == v2) {
+				f.val = f.val - cf_P; 
+				}
+			});
+			
+			cf1.forEach(function(f){ 
+				if (f.v1 == v1 && f.v2 == v2) {
+				f.val = f.val - cf_P; 
+				}
+			});
+			
+			cf2.forEach(function(f){ 
+				if (f.v2 == v1 && f.v1 == v2) {
+				f.val =f.val + cf_P; 
+				}
+			});
+						
+			}
+			console.log(cf1);
+		}
+		
+		//else { break;}
+	//}
+	
 }
 
 
 $(function() {
-	Ford_Fulkerson(G[0], G[length-1]);
+	Ford_Fulkerson(G[0], G[G.length-1]);
 });
+

ebfe1ce482aa8a594328f859537d778cd5dd7c32
wer4ty <roma0152@bitbucket.org>
2019-01-18 16:09:35 +0200

    BFS animation with table and graph

--- js/bfs.js
+++ js/bfs.js
@@ -1,5 +1,9 @@
 // 0. Graph object represent as list of members
  
+ 
+// TODO Edit graph by user 
+// check array nodes and edges why they do not update after changes
+ 
 let G = [
 	{"name": 's', "neighbors": ['a', 'b', 'c'], "v_ind": 0 },
 	{"name": 'a', "neighbors": ['b', 'd'], "v_ind": 1 },
@@ -11,35 +15,104 @@ let G = [
 	{"name": 'g', "neighbors": ['f'], "v_ind": 7 }
 ];
 
-let nodes, edges;
-let vis_animation = 0;
+let finish_flag = false, startNode;
+let nodes, edges, t_head, tr;
+let vis_animation = 0, table_animation = 0, tr_id = 0;
+let initial_color = "#7ebde5", initial_border_color = "#72a6f9";
 
 $(function() {
 
 	viz();
-	bsf(G, G[0]);
+		
+	// controls
+	$("#start").click(function(){
+		reset();
+		rebildGraphViaUserChanges();
+		let startNodeName = prompt("Enter name of start vertice ?"); 
+		startNode = search(G, startNodeName);
+		if (startNode != null) {
+			setTimeout(function(){ bsf(G, startNode); }, 1000);
+			
+		}
+		else {
+			alert("Not found vertice "+startNodeName+" in current graph");
+		}
+	});
+	
+	
 	
 });
+
+function reset() {
+	if (finish_flag) {
+		vis_animation = 0; table_animation= 0; tr_id=0;
+		$("#tbody tr").remove();
+		finish_flag = false;
+	}
+}
+
+// 0. Rebild Graph via user changes
+function rebildGraphViaUserChanges() {
+	G = [];
+	
+
+	function NodeIdToLabel(id) {
+		let tmp;
+		nodes.forEach(function(n){
+			if (n.id == id) { tmp = n.label;  return n.label; }
+		});
+		return tmp;
+	}
+	
+	function searchIdInEdges(id) {
+		let res = [];
+		edges.forEach(function(e){
+			if (e.from == id) { res.push(e.to); }
+		});
+		return res;
+	}
+	
+	let obj, counter=0;
+	
+	nodes.forEach(function(n){  
+			obj = {name: undefined, "neighbors": [] };
+			obj.name = n.label;
+			
+			let tmp_neibors = searchIdInEdges(n.id);
+			for (let j=0; j<tmp_neibors.length; j++) {
+				obj.neighbors.push(NodeIdToLabel(tmp_neibors[j]));
+			}
+			G[counter] = obj;
+			counter++;
+	
+	});
+	console.log(G);
+	tableHead();
+}
 
 
 //1. Visualization begin
 function viz() {
 
+// GRAPH ---------------------------------------------------------------------------------------------------------
   // create an array with nodes
    nodes = new vis.DataSet([]);
    edges = new vis.DataSet([]);
 
+   
   for (let i=0; i<G.length; i++) {
-  	nodes.add( {id: i, label: G[i].name, font: { size:  18 }, widthConstraint: { minimum: 100 } } );
+  	nodes.add( {id: i.toString(), label: G[i].name, font: { size:  11 }, widthConstraint: { minimum: 60 }, physics: false, color:{ background:initial_color, border:initial_border_color } }
+	);
   }
 
   // create an array with edges
   for (let i=0; i<G.length; i++) {
   	for (let j=0; j<G[i].neighbors.length; j++) {
   		let to_ind = search(G, G[i].neighbors[j]);
-  		edges.add( {from: i, to: to_ind.v_ind, arrows:'to'} );
+  		edges.add( {from: i.toString(), to: to_ind.v_ind.toString(), arrows:'to', length: 40} );
   	}
   }
+  
 
   // create a network
   var container = document.getElementById('mynetwork');
@@ -49,30 +122,89 @@ function viz() {
   };
 
   var options = {
+	manipulation: { enabled: true,
+		addNode: function(nodeData,callback) {
+			var nodeName = prompt("Name of new node ?");
+		  nodeData.label = nodeName;
+		  nodeData.physics = false;
+		  callback(nodeData);
+		},
+		
+		addEdge: function(edgeData,callback) {
+			var directed = confirm("Directed Edge ?");
+		  if (directed) { edgeData.arrows = 'to'; }
+		  if (edgeData.from === edgeData.to) {
+			var r = confirm("Do you want to connect the node to itself?");
+			if (r === true) {
+			  callback(edgeData);
+			}
+		  }
+		  else {
+			callback(edgeData);
+		  }
+		}
 
-    // Enable this to make the endpoints smaller/larger
-    edges: {
-      arrows: {
-        to: {
-          scaleFactor: 1
-        }
-      }
-    }
-
+	}
   };
 
   var network = new vis.Network(container, data, options);
+  
+  let parentWidth = $("#mynetwork").parent().width();
+  $("#mynetwork").resizable({ maxWidth: parentWidth, minHeight: 200, minWidth: 200 });
+
+   tableHead();
 } 
 
-function repeaint(id, color, fontColor) {
+function repeaint(id, color, fontColor, text, borderColor) {
 	vis_animation++;
 	setTimeout(function(){
 			nodes.update({
 				id: id, 
+				label: text,
 				font: { color:  fontColor },
-			 	color:{ background:color, border:color,highlight:{background:'red',border:'blue'}}
+			 	color:{ background:color, border:borderColor,highlight:{background:'red',border:'blue'}}
 			 	 });
-		}, vis_animation*1200);
+		}, vis_animation*1000);
+}
+
+function repeaintAll(id, color, fontColor, text, borderColor) {
+			nodes.update({
+				id: id, 
+				label: text,
+				font: { color:  fontColor },
+			 	color:{ background:color, border:borderColor,highlight:{background:'red',border:'blue'}}
+			 	 });
+}
+
+function tableHead() {
+  $("#thead tr").remove();
+  t_head = "<tr><th scope='col'>#</th>";
+  for (let i=0; i<G.length; i++) { 
+	t_head+="<th scope='col'>"+G[i].name+"</th>";
+	tr+= "<td>-<br>-</td>";	
+  }
+  t_head += "</tr>";
+  tr += "</tr>";
+  
+  $("#thead").append(t_head);
+}
+
+function insertIntoTable() {
+	$("#tbody #"+tr_id).delay(tr_id*1500).show( "highlight", 1000);
+	let di, pi;
+	tr_id++;
+	tr = "<tr id="+tr_id+"><th scope='row'>d<br>&pi;</th>";
+	for (let i =0; i<G.length; i++) {
+		
+		if (G[i].d == Infinity) { di = "Infinity";  }
+		else { di = G[i].d; }
+		if (G[i].pi == null) { pi = "Nil";  }
+		else { pi = G[i].pi.name; }
+		
+		tr += "<td>"+di+"<br>"+pi+"</td>";
+	}
+	tr += "</tr>";
+	$("#tbody").prepend(tr);
 }
 
 //2. Algorithms BFS (Bread First Search)  
@@ -94,15 +226,17 @@ function bsf(G, s) {
 		G[i].color = 'W' // white
 		G[i].pi = null;
 
-		repeaint(i, "#fff", "#000");
-	} 
-
+		repeaintAll(i, "#fff", "#000", G[i].name+"\nd = INF \nPi = NIL", "#aaa");	
+	}
+	insertIntoTable();
+	
+	
 	s.d = 0;
 	s.color = 'G'; // gray
 	Q.enqueue(s);
 
 	let t_ind = search(G, s.name);
-	repeaint(t_ind.v_ind, "#777", "#000");
+	repeaint(t_ind.v_ind, "#777", "#000", t_ind.name+"\nd = 0 \npi = NIL", "#777");
 
 	let steps = 1;
 	// 2.3 Alrorithms cycle
@@ -116,15 +250,21 @@ function bsf(G, s) {
 				tmp_node.color = 'G';
 				Q.enqueue(search(G, v));
 
-				repeaint(tmp_node.v_ind, "#777", "#000");
+				repeaint(tmp_node.v_ind, "#777", "#000", tmp_node.name+"\nd = "+tmp_node.d+" \npi = "+tmp_node.pi.name, "#777");
+				
 			}
 		});
 		u.color = 'B';
 		steps += 1;
 
-		repeaint(u.v_ind, "#333", "#fff"); 
+		let pi_name;
+		if (u.pi == null) { pi_name = "NIL"; }
+		else { pi_name = u.pi.name; }
+		repeaint(u.v_ind, "#333", "#fff", u.name+"\nd = "+u.d+" \npi = "+pi_name, "#333");
+		insertIntoTable();
 	}
 	console.log(G);
+	finish_flag = true;
 }
 
 //search in array helper function
